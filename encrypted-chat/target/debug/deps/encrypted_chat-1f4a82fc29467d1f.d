2207bd5e6d2ccae20727f28cedf61f74e718b07b
Adam Puchała <adam.puchala@comarch.com>
2021-09-10 22:45:06 +0200

    refactor: removed duplicated p2p module

--- encrypted-chat/target/debug/deps/encrypted_chat-1f4a82fc29467d1f.d
+++
@@ -1,9 +0,0 @@
-/Users/adam/Projects/rust/encrypted-chat/target/debug/deps/encrypted_chat-1f4a82fc29467d1f.rmeta: src/main.rs src/remote_peer.rs src/peers_file_handler.rs
-
-/Users/adam/Projects/rust/encrypted-chat/target/debug/deps/encrypted_chat-1f4a82fc29467d1f.d: src/main.rs src/remote_peer.rs src/peers_file_handler.rs
-
-src/main.rs:
-src/remote_peer.rs:
-src/peers_file_handler.rs:
-
-# env-dep:PWD=/
--- encrypted-chat/target/debug/deps/encrypted_chat.d
+++
@@ -1,9 +0,0 @@
-/Users/adam/Projects/rust/encrypted-chat/target/debug/deps/encrypted_chat: src/main.rs src/remote_peer.rs src/peers_file_handler.rs
-
-/Users/adam/Projects/rust/encrypted-chat/target/debug/deps/encrypted_chat.d: src/main.rs src/remote_peer.rs src/peers_file_handler.rs
-
-src/main.rs:
-src/remote_peer.rs:
-src/peers_file_handler.rs:
-
-# env-dep:PWD=/Users/adam/Projects/rust/encrypted-chat
